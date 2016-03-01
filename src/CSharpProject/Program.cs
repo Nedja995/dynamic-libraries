fbb7692ce9858bd45b06bbcd7bb88ab7a88d2a79
Nedja <nedjapejasinovic@gmail.com>
2016-03-01 18:50:44 +0100

    c# project

---
+++ src/CSharpProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class Program
+    {
+        [DllImport("FirstLibrarySH.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Add(int a, int b);
+
+        static void Main(string[] args)
+        {
+            int a = Add(3, 5);
+            int b = 3;
+        }
+    }
+}
